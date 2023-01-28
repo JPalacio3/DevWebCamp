dfd5e2a86e741b32e563ebecd2158418ed972ae9
JPalacio <Joelpalacio630@gmail.com>
2023-01-27 23:56:01 -0600

    Añadiendo la funcionalidad con JS y estilos del formulario

---
+++ src/js/tags.js
@@ -0,0 +1,52 @@
+(function () {
+
+    const tagsInput = document.querySelector('#tags_input');
+
+    if (tagsInput) {
+
+        let tagsDiv = document.querySelector('#tags');
+        let tagsInputHidden = document.querySelector('[name="tags"]');
+
+        let tags = [];
+        //Escuchar los cambios en el input
+        tagsInput.addEventListener('keypress',guardarTag);
+
+        function guardarTag(e) {
+
+            if (e.keyCode === 44) {
+
+                //Evita campos vacíos
+                if (e.target.value.trim() === '' || e.target.value <= 1) { return }
+
+                // Guardar los Tags
+                e.preventDefault();
+                tags = [ ...tags,e.target.value.trim() ];
+                tagsInput.value = '';
+
+                mostrarTags();
+            }
+        }
+
+        function mostrarTags() {
+            tagsDiv.textContent = '';
+            tags.forEach(tag => {
+                const etiqueta = document.createElement('LI');
+                etiqueta.classList.add('formulario__tag');
+                etiqueta.textContent = tag;
+                etiqueta.ondblclick = eliminarTag;
+                tagsDiv.appendChild(etiqueta);
+            })
+            actualizarInputHidden();
+        }
+
+        function eliminarTag(e) {
+            e.target.remove();
+            tags = tags.filter(tag => tag !== e.target.textContent);
+            actualizarInputHidden();
+        }
+
+        function actualizarInputHidden() {
+            tagsInputHidden.value = tags.toString();
+        }
+    }
+})()  // IIFE
