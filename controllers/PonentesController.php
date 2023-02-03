1417264bbcf918d46810f3b06eb880ff214d61a4
JPalacio <Joelpalacio630@gmail.com>
2023-02-02 20:25:02 -0600

    Listando los ponentes de la base de datos y añadiendo estilos a la tabla

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,27 +3,80 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController
 {
 
     public static function index(Router $router)
     {
+        $ponente = Ponente::all();
+
+
 
 
 
         $router->render('admin/ponentes/index', [
             'titulo' => 'Ponentes / Conferencistas',
+            'ponentes' => $ponente,
         ]);
     }
+
+
 
     public static function crear(Router $router)
     {
         $alertas = [];
+        $ponente = new Ponente;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // leer imagen
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+
+                // Generamos una nueva carpeta para almacenar las imágenes de los ponentes
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if (!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0777, true);
+                }
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 90);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 90);
+
+                // Generar el nombre para cada imagen?
+                $nombre_imagen = md5(uniqid(rand(), true));
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
+            // Convertir el arreglo de redes sociales a String para que la función sanitizar pueda ejecutarse y se pueda mandar el archivo sanitizado a la base de datos
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+
+            $ponente->sincronizar($_POST);
+
+            // Validar
+            $alertas = $ponente->validar();
+
+            // Guardar el registro con la imagen
+            if (empty($alertas)) {
+                // Guardar las imágenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
+
+                // Guardar en la base de datos
+                $resultado = $ponente->guardar();
+
+                if ($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
 
         $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
             'alertas' => $alertas,
+            'ponente' => $ponente,
         ]);
     }
 }
--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -6,3 +6,58 @@
         Añadir Ponente
     </a>
 </div>
+
+<!-- Tabla para mostrar todos los ponentes -->
+
+<div class="dashboard__contenedor">
+    <?php if (!empty($ponentes)) { ?>
+        <table class="table">
+
+            <thead class="table__thead">
+                <tr>
+                    <th class="table__th">Nombre </th>
+                    <th class="table__th">Ubicación </th>
+                    <th class="table__th">Experiencia </th>
+                    <th class="table__th"> </th>
+                </tr>
+            </thead>
+
+            <tbody class="table__tbody">
+                <?php
+                foreach ($ponentes as $ponente) { ?>
+                    <tr class="table__tr">
+
+                        <td class="table__td"><?php echo $ponente->nombre . ' ' . $ponente->apellido; ?> </td>
+                        <td class="table__td"><?php echo $ponente->ciudad . ', ' . $ponente->pais; ?> </td>
+                        <td class="table__td"><?php echo $ponente->tags; ?> </td>
+
+                        <td class="table__td--acciones">
+
+                            <a class="table__accion table__accion--editar" href="/admin/ponentes/editar?id=<?php echo $ponente->id; ?>"> <i class="fa-solid fa-user-pen"></i>Editar
+                            </a>
+
+                            <form action="" class="table__formulario">
+                                <button class="table__accion table__accion--eliminar" type="submit">
+                                    <i class="fa-solid fa-circle-xmark"></i>
+                                    Eliminar
+                                </button>
+                            </form>
+
+                        </td>
+
+                    </tr>
+                <?php } ?>
+
+
+
+
+
+            </tbody>
+
+        </table>
+
+    <?php } else { ?>
+        <p class="text-center"> No Hay Ponentes Aún</p>
+    <?php } ?>
+
+</div>
